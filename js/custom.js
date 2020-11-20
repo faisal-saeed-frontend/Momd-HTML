900e6c6296e7777bcf260fb6c1a52b97338a6c32
faisalsaeed <faisalsaeed128@gmail.com>
2020-11-20 16:28:39 +0500

    first code

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+
+// Custom JS 
+
+/*
+* @author: Faisal Saeed
+*/
+
+$(function SieveOfEratosthenesCached(n, cache) {
+  $('#dreams-carousel').owlCarousel({
+    loop: false,
+    margin: 20,
+    stagePadding: 0,
+    autoplay: false,
+    nav: true,
+    dots: false,
+    rtl: false,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 0,
+        margin: 30
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      },
+      1645: {
+        items: 6
+      }
+
+    }
+  })
+});
+
+
+
+
+
+
+
+
+
